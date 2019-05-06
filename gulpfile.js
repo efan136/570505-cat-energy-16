c2076e90c365dfef08110e72ef1cee3503eba424
Andrew <andrei_efremov@inbox.ru>
2019-05-06 18:28:25 +0400

    улучшаем скорость загрузки

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,16 @@ var rename = require("gulp-rename");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
 var del = require("del");
 var server = require("browser-sync").create();
 
+gulp.task("uglify", function () {
+  return gulp.src("source/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -62,6 +69,7 @@ gulp.task("html", function () {
     .pipe(posthtml([
       include()
     ]))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 });
 
@@ -73,7 +81,6 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
     "source/*.ico"
    ], {
    base: "source"
@@ -86,7 +93,8 @@ gulp.task("build", gulp.series (
   "copy",
   "css",
   "sprite",
-  "html"
+  "html",
+  "uglify"
   ));
 
 gulp.task("server", function () {
