211c5d8037fadcbd636deb02fa073a92fbdb642f
Andrew <andrei_efremov@inbox.ru>
2019-06-14 17:12:15 +0400

    поправил пути картинок перенес js в файл js

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 var navMain = document.querySelector(".main-nav");
     var navBtn = document.querySelector(".main-nav__menu-btn");
+    var rng = document.getElementById('r1'); //rng - это Input
+    var div = document.getElementById('before'); // div - блок test
     navMain.classList.remove("main-nav--nojs");
     navBtn.addEventListener("click", function() {
       if(navMain.classList.contains("main-nav--closed")) {
@@ -10,3 +12,6 @@ var navMain = document.querySelector(".main-nav");
         navMain.classList.remove("main-nav--opened");
       }
     })
+function fun1() {
+  div.style.width = rng.value +'%';
+}
